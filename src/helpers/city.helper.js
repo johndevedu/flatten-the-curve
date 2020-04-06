3c54e248815fd98a88cf2d6a9c1c6b0b31475ab0
John Lee <johnwoolee@gmail.com>
2020-04-06 13:50:27 -0700

    fix missing date range

--- src/helpers/city.helper.js
+++ src/helpers/city.helper.js
@@ -24,25 +24,48 @@ export const getCityMultiple = async (cityList) => {
   }
 
 
-  const dates = parseDate(results[0])
-  const seriesCollection = parseSeries(results)
+  const {dateRange, longestSeries} = parseDate(results)
+  const seriesCollection = parseSeries(results, longestSeries)
   return {
-    dates,
+    dates: dateRange,
     seriesCollection
   };
 }
 
-const parseDate = (series) => {
-  const dates = series.map(item => moment(item.date).format('M/D'))
-  return dates;
+const parseDate = (seriesCollection) => {
+  let longestSeries = [];
+  seriesCollection.forEach(series => series.length > longestSeries.length && (longestSeries = series))
+  
+  const dateRange = longestSeries.map(item => moment(item.date).format('M/D'))
+  return {
+    dateRange,
+    longestSeries
+  };
 }
 
-const parseSeries = (originalSeriesCollection) => {
+const parseSeries = (originalSeriesCollection, dateRange) => {
   const parsedSeriesCollection = []
   originalSeriesCollection.forEach(series => {
     const parsedSeries = []
 
-    series.forEach(data => parsedSeries.push(data.infections))
+    let seriesIndex = 0;
+    for (let i = 0; i < dateRange.length ; i ++){
+      const seriesDate = series[seriesIndex].date;
+      const infections = series[seriesIndex].infections;
+
+      if (seriesIndex === 0 && (seriesDate !== dateRange[i].date)){
+        parsedSeries.push(0)
+        continue
+      }
+      if (seriesIndex > 0) {
+        parsedSeries.push(infections)
+      }
+      else if (seriesDate === dateRange[i].date) {
+        parsedSeries.push(infections)
+      } 
+      
+      seriesIndex++;
+    }
 
     const city = series[series.length - 1].city
     parsedSeriesCollection.push({
